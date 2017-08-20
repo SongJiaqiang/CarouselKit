c1cedda8dc90ddb72cc71932aeb13a050ce1340b
Jarvis <sjq2150@gmail.com>
2017-08-20 18:41:29 +0800

    添加CarouselKit

---
+++ CarouselKitSample/CarouselKitSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarouselKitSample
+//
+//  Created by Jarvis on 20/08/2017.
+//  Copyright © 2017 JQTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarouselKitSample/CarouselKitSample/ViewController.m
@@ -0,0 +1,130 @@
+//
+//  ViewController.m
+//  CarouselKitSample
+//
+//  Created by Jarvis on 20/08/2017.
+//  Copyright © 2017 JQTech. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "iCarousel.h"
+
+@interface ViewController () <iCarouselDataSource, iCarouselDelegate>
+
+@property (weak, nonatomic) IBOutlet iCarousel *carouselView;
+
+@property (nonatomic, strong) NSMutableArray *items;
+
+@end
+
+@implementation ViewController
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    //set up data
+    //your carousel should always be driven by an array of
+    //data of some kind - don't store data in your item views
+    //or the recycling mechanism will destroy your data once
+    //your item views move off-screen
+    self.items = [NSMutableArray array];
+    for (int i = 0; i < 100; i++)
+    {
+        [_items addObject:@(i)];
+    }
+}
+
+- (void)dealloc
+{
+    //it's a good idea to set these to nil here to avoid
+    //sending messages to a deallocated viewcontroller
+    //this is true even if your project is using ARC, unless
+    //you are targeting iOS 5 as a minimum deployment target
+    _carouselView.delegate = nil;
+    _carouselView.dataSource = nil;
+}
+
+#pragma mark -
+#pragma mark View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //configure carousel
+    _carouselView.type = iCarouselTypeCoverFlow2;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    
+    //free up memory by releasing subviews
+    _carouselView = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+#pragma mark -
+#pragma mark iCarousel methods
+
+- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel
+{
+    //return the total number of items in the carousel
+    return [_items count];
+}
+
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
+{
+    UILabel *label = nil;
+    
+    //create new view if no view is available for recycling
+    if (view == nil)
+    {
+        //don't do anything specific to the index within
+        //this `if (view == nil) {...}` statement because the view will be
+        //recycled and used with other index values later
+        view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200.0f, 200.0f)];
+        view.backgroundColor = [UIColor lightGrayColor];
+        ((UIImageView *)view).image = [UIImage imageNamed:@"page.png"];
+        view.contentMode = UIViewContentModeCenter;
+        
+        label = [[UILabel alloc] initWithFrame:view.bounds];
+        label.backgroundColor = [UIColor clearColor];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.font = [label.font fontWithSize:50];
+        label.tag = 1;
+        [view addSubview:label];
+    }
+    else
+    {
+        //get a reference to the label in the recycled view
+        label = (UILabel *)[view viewWithTag:1];
+    }
+    
+    //set item label
+    //remember to always set any properties of your carousel item
+    //views outside of the `if (view == nil) {...}` check otherwise
+    //you'll get weird issues with carousel item content appearing
+    //in the wrong place in the carousel
+    label.text = [_items[index] stringValue];
+    
+    return view;
+}
+
+- (CGFloat)carousel:(iCarousel *)carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value
+{
+    if (option == iCarouselOptionSpacing)
+    {
+        return value * 1.1;
+    }
+    return value;
+}
+
+
+
+@end
