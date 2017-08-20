0bcd8a6a1830a8b9e0d1d47046cd1967cf696985
Jarvis <sjq2150@gmail.com>
2017-08-20 18:41:29 +0800

    添加CarouselKit

---
+++ CarouselKitSample/CarouselKit/CarouselKit.h
@@ -0,0 +1,21 @@
+//
+//  CarouselKit.h
+//  CarouselKit
+//
+//  Created by Jarvis on 20/08/2017.
+//  Copyright © 2017 JQTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iCarousel.h"
+
+//! Project version number for CarouselKit.
+FOUNDATION_EXPORT double CarouselKitVersionNumber;
+
+//! Project version string for CarouselKit.
+FOUNDATION_EXPORT const unsigned char CarouselKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarouselKit/PublicHeader.h>
+
+
---
+++ CarouselKitSample/CarouselKitSample/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  CarouselKitSample
+//
+//  Created by Jarvis on 20/08/2017.
+//  Copyright © 2017 JQTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
